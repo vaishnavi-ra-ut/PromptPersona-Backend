c10da24b3536dd57404093af2798f06120e937e7
Vaishnavi Raut <rautvaishnavi29@gmail.com>
2025-07-30 15:05:31 +0530

    Changed the Frontend URL in CORS

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json()); 
 
 app.use(cors({
-  origin: "http://localhost:5173",  
+  origin: "https://prompt-persona-frontend.vercel.app",  
   credentials: true    
 }));
 
